0d2ba6d2d730918a0b89afa323d14d866deb06aa
Maximilian Gewers <42013351+MaximilianGewers@users.noreply.github.com>
2025-11-20 04:18:52 +0000

    chore: nav and footer

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,18 @@ import './stimulus_bootstrap.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Flowbite with Trubo
+import { initFlowbite } from 'flowbite';
+
+document.addEventListener('turbo:render', () => {
+    console.log('Turbo render event detected');
+    initFlowbite();
+    console.log('Flowbite initialized on turbo:render');
+});
+
+document.addEventListener('turbo:frame-render', () => {
+    console.log('Turbo frame render event detected');
+    initFlowbite();
+    console.log('Flowbite initialized on turbo:frame-render');
+});
